69d708aefedf48aa1a98798864422969c4bc3138
zhaoshengjun <zhao.shengjun@gmail.com>
2015-06-14 22:55:25 +1000

    minor changes

--- Solution1.py
+++ Solution1.py
@@ -2,13 +2,15 @@ import sys
 import re
 import os
 
+# get the start directory
+start = sys.argv[1]
 # get the pattern from the command line arguments
-pattern = sys.argv[1]
+pattern = sys.argv[2]
 # convert them into regular expression
 expr = re.compile(pattern)
 # traverse the directories for all files
-for root, dirs, files in os.walk('h:/Playground/'):
+for root, dirs, files in os.walk(start):
     # if a file matches the pattern, print its name
     for fname in files:
-        if expr.search(fname):
+        if expr.match(fname):
             print(os.path.join(root, fname))
